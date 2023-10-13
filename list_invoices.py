26fcd4218898144818da97d038f27a70f51c2b03
marijn <marijn@serraict.com>
2023-10-13 15:27:54 +0200

    Get invoice JSON data

--- list_invoices.py
+++ list_invoices.py
@@ -1,1 +1,22 @@
-print("Ready to list invoices...")
+import os
+import requests
+
+query = """
+{
+  findInvoices(invoiceLines: {containsAny: {}}) {
+    id
+    name
+    invoiceLines {
+      name
+    }
+  }
+}
+"""
+
+url = os.environ["SPACE_URL"]
+token = os.environ["FIBERY_API_TOKEN"]
+
+headers = {"Content-Type": "application/json", "Authorization": "Token " + token}
+
+response = requests.post(url, json={"query": query}, headers=headers)
+print(response.json())
