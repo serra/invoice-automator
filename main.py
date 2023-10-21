81c3161b47bee3bf3ab41fa370b070f2cfe75cd2
marijn <marijn@serraict.com>
2023-10-21 17:12:24 +0200

    Add sum command

--- main.py
+++ main.py
@@ -24,7 +24,12 @@ invoice_query = """
     """
 
 
-@click.command()
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
 @click.option("--state", default="Ready", help="Filter invoices by state")
 @click.option("--url", envvar="SPACE_URL", help="Fibery Space URL")
 @click.option("--token", envvar="FIBERY_API_TOKEN", help="Fibery API Token")
@@ -41,5 +46,20 @@ def list_invoices(state, url, token):
         )
 
 
+@cli.command()
+@click.option("--state", default="Ready", help="Filter invoices by state")
+@click.option("--url", envvar="SPACE_URL", help="Fibery Space URL")
+@click.option("--token", envvar="FIBERY_API_TOKEN", help="Fibery API Token")
+def sum_invoices(state, url, token):
+    query = invoice_query % state
+
+    headers = {"Content-Type": "application/json", "Authorization": "Token " + token}
+
+    response = requests.post(url, json={"query": query}, headers=headers)
+    invoice_data = response.json()["data"]["findInvoices"]
+    total_amount = sum([invoice["totalAmount"] for invoice in invoice_data])
+    print(f"Total amount of invoices in {state} state is {total_amount}")
+
+
 if __name__ == "__main__":
-    list_invoices()
+    cli()
