550c9da9abb2a8f590749208401e26d155811516
marijn <marijn@serraict.com>
2023-10-25 10:34:09 +0200

    Use keyring to retrieve and store access token

--- gmail.py
+++ gmail.py
@@ -1,27 +1,30 @@
+import json
 import os
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+import keyring
+
+INVOICE_AUTOMATOR_TOKEN_KEY = "SerraICTInvoiceAutomatorToken"
+SYSTEM_NAME = "Serra ICT Invoice Automator"
 
 
 def build_service(scopes):
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", scopes)
+    token = keyring.get_password(SYSTEM_NAME, INVOICE_AUTOMATOR_TOKEN_KEY)
+    if token:
+        token_json = json.loads(token)
+        creds = Credentials.from_authorized_user_info(token_json, scopes)
+
     # If there are no (valid) credentials available, let the user log in.
+    # This is necessary because we are using email on behalf of an actual human.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file("credentials.json", scopes)
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
+        keyring.set_password(SYSTEM_NAME, INVOICE_AUTOMATOR_TOKEN_KEY, creds.to_json())
 
-    # Call the Gmail API and return the service
     return build("gmail", "v1", credentials=creds)
