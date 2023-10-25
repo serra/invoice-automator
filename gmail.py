17b296d6f072f1a9182ee26a5d27a8b3e14acb1b
marijn <marijn@serraict.com>
2023-10-25 11:01:11 +0200

    Retrieve gmail client configuration from keyring

--- gmail.py
+++ gmail.py
@@ -6,8 +6,26 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import keyring
 
-INVOICE_AUTOMATOR_TOKEN_KEY = "SerraICTInvoiceAutomatorToken"
+
+INVOICE_AUTOMATOR_CLIENT_SECRETS_KEY = "SerraICTInvoiceAutomatorGoogleClientSecrets"
+INVOICE_AUTOMATOR_TOKEN_KEY = "SerraICTInvoiceAutomatorGoogleToken"
 SYSTEM_NAME = "Serra ICT Invoice Automator"
+
+
+def get_client_secrets():
+    client_secrets = keyring.get_password(
+        SYSTEM_NAME, INVOICE_AUTOMATOR_CLIENT_SECRETS_KEY
+    )
+    if client_secrets:
+        return json.loads(client_secrets)
+    else:
+        # raise exception if client secrets not found with system name and key
+        raise Exception(
+            f"Client secrets not found with system name '{SYSTEM_NAME}' "
+            + f"and key '{INVOICE_AUTOMATOR_CLIENT_SECRETS_KEY}'. "
+            + "Please add the client configuration in json format to your platform's keyring. "
+            + "You can download the client configuration from https://console.cloud.google.com/apis/credentials. "
+        )
 
 
 def build_service(scopes):
@@ -23,7 +41,8 @@ def build_service(scopes):
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", scopes)
+            client_secrets = get_client_secrets()
+            flow = InstalledAppFlow.from_client_config(client_secrets, scopes=scopes)
             creds = flow.run_local_server(port=0)
         keyring.set_password(SYSTEM_NAME, INVOICE_AUTOMATOR_TOKEN_KEY, creds.to_json())
 
